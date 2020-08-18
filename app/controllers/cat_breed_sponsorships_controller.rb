c89ead6ce1686729b4bea670c955afa8308c05b7
haleyvferro <haleyvferro@gmail.com>
2020-08-18 13:06:54 -0500

    trying to get cat breeds to show for patron, need to fix associations

--- app/controllers/cat_breed_sponsorships_controller.rb
+++ app/controllers/cat_breed_sponsorships_controller.rb
@@ -1,2 +1,15 @@
 class CatBreedSponsorshipsController < ApplicationController
+
+    def index
+        catBreedSponsorships = CatBreedSponsorship.all
+        render json: catBreedSponsorships
+    end
+
+    def show
+        catBreedSponsorship = CatBreedSponsorship.find(params[:id])
+        render json: catBreedSponsorship
+    end
+
+    
+
 end
--- app/models/cat_breed_sponsorship.rb
+++ app/models/cat_breed_sponsorship.rb
@@ -2,5 +2,5 @@ class CatBreedSponsorship < ApplicationRecord
 
     # belongs_to :patron
     # belongs_to :cat_breed
-    
+
 end
