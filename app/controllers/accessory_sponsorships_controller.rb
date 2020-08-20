9a0ca26d36137473069a59f63836c200f81afc24
haleyvferro <haleyvferro@gmail.com>
2020-08-20 17:06:28 -0500

    fixed accessory sponsorship create

--- app/controllers/accessory_sponsorships_controller.rb
+++ app/controllers/accessory_sponsorships_controller.rb
@@ -1,35 +1,35 @@
 class AccessorySponsorshipsController < ApplicationController
 
     def index
-        accessory_sponsorships = AccessorySponsorship.all
-        render json: accessory_sponsorships
+        accsSponsorships = AccessorySponsorship.all
+        render json: accsSponsorships
     end
 
     def show
-        accessory_sponsorship = AccessorySponsorship.find(params[:id])
+        accsSponsorship = AccessorySponsorship.find(params[:id])
         render json: accsSponsorship
     end
 
     def create
-        accessory = Accessory.create(accs_params)
+        accsSponsorship = AccessorySponsorship.create(accs_params)
         render json: accsSponsorship
     end
 
     def update
-        accessory_sponsorship = AccessorySponsorship.find(params[:id])
-        accessory_sponsorship.update(accs_params)
+        accsSponsorship = AccessorySponsorship.find(params[:id])
+        accsSponsorship.update(accs_params)
         render json: accsSponsorship
     end
 
     def destroy
-        accessory_sponsorship = AccessorySponsorship.find(params[:id])
-        accessory_sponsorship.delete
+        accsSponsorship = AccessorySponsorship.find(params[:id])
+        accsSponsorship.delete
     end
 
     private
 
     def accs_params
-        params.require(:accessory_sponsorship).permit(:amount)
+        params.require(:accessory_sponsorship).permit(:amount, :patron_id, :accessory_id)
     end
 
 end
